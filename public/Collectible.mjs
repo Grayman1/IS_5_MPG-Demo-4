eed7a7482aa2fc32354a991f9b4bf6322f3e845b
Grayman1 <54072430+Grayman1@users.noreply.github.com>
2021-08-03 16:17:52 -0400

    Initial Commit
    Initial commit from repo unzipped local folder

---
+++ public/Collectible.mjs
@@ -0,0 +1,23 @@
+import { dimension } from './dimension.mjs';
+
+class Collectible {
+  constructor({x, y, value, id}) {
+    this.x = x;
+    this.y = y;
+    this.value = value;
+    this.id = id;
+    this.width = dimension.COIN_WIDTH;
+    this.height = dimension.COIN_HEIGHT;
+  }
+
+}
+
+/*
+  Note: Attempt to export this for use
+  in server.js
+*/
+try {
+  module.exports = Collectible;
+} catch(e) {}
+
+export default Collectible;
